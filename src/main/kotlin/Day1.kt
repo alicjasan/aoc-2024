ce8a7d0ea0c09e556878ccef3c2249cc25c51c6d
Alicja Sanestrzik <alicja.sanestrzik@gmail.com>
2024-12-01 14:36:41 +0100

    First day pt 2

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -4,12 +4,25 @@ import org.example.util.readTwoColumnsOfNumbers
 
 fun main() {
     val lists = readTwoColumnsOfNumbers("day1")
+    println(findDistancesSum(lists))
+    println(calculateSimilarityScore(lists.first.sorted(), lists.second.sorted()))
+}
+
+fun findDistancesSum(lists: Pair<List<Int>, List<Int>>): Int {
     val firstListSorted = lists.first.sorted()
     val secondListSorted = lists.second.sorted()
 
     val result = firstListSorted.mapIndexed { index, number ->
-       kotlin.math.abs(number - secondListSorted[index])
+        kotlin.math.abs(number - secondListSorted[index])
     }.sum()
 
-    println(result)
+    return result
 }
+
+fun calculateSimilarityScore(sortedList1: List<Int>, sortedList2: List<Int>): Int {
+    return sortedList1.sumOf { element ->
+        val count = sortedList2.takeWhile { it <= element }.count { it == element }
+        element * count
+    }
+}
+
