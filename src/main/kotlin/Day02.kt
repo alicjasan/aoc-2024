a82ba25c96340979bd65b2bfd24ac9adc54c1428
Alicja Sanestrzik <alicja.sanestrzik@gmail.com>
2024-12-02 10:55:36 +0100

    Day 2 pt 2

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -15,14 +15,8 @@ private fun findDistance(a: Int, b: Int): Int {
 private fun countSafeRecords(linesOfNumbers: List<List<Int>>): Int {
     var safeRecords = 0
     linesOfNumbers.forEach { line ->
-        if (isMonotonic(line)) {
-            val allDistancesValid = line.zipWithNext().all { (a, b) ->
-                val distance = findDistance(a, b)
-                distance in 1..3
-            }
-            if (allDistancesValid) {
-                safeRecords++
-            }
+        if (isSafe(line) || isSafeWithOneRemoval(line)) {
+            safeRecords++
         }
     }
     return safeRecords
@@ -37,3 +31,16 @@ fun isMonotonic(list: List<Int>): Boolean {
     return increasing || decreasing
 }
 
+private fun isSafe(line: List<Int>): Boolean {
+    return isMonotonic(line) && line.zipWithNext().all { (a, b) ->
+        val distance = findDistance(a, b)
+        distance in 1..3
+    }
+}
+
+private fun isSafeWithOneRemoval(line: List<Int>): Boolean {
+    return line.indices.any { index ->
+        val modifiedLine = line.toMutableList().apply { removeAt(index) }
+        isSafe(modifiedLine)
+    }
+}
